f40ce139b4ef94ebfc7473cdce094f95e86b29fc
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2025-01-20 01:11:40 +0100

    Create buzzerManager.js with basic code template to handle buzzer

---
+++ js/buzzerManager.js
@@ -0,0 +1,104 @@
+const socket = new WebSocket('ws://localhost:5000/buzzers-event');
+
+socket.onopen = () => {
+    console.log('Connecté au WebSocket');
+    socket.send(JSON.stringify({ protocol: 'json', version: 1 }) + "\u001E");
+};
+
+socket.onmessage = (event) => {
+    const messages = event.data.split("\u001E");
+
+    messages.forEach(rawMessage => {
+        if (rawMessage.trim()) {
+            const message = JSON.parse(rawMessage);
+
+            if (message.type == 1) {
+                document.dispatchEvent(new CustomEvent(message.target, {arguments: message.arguments}));
+            } else {
+                console.log(message);
+            }
+        }
+    });
+};
+
+document.addEventListener('playerHasTheHand', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerIsLocked', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerPlayed', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerHasReleasedTheHand', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerIsReseted', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerIsEnrolled', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerListing', event => {
+    console.log(event);
+});
+
+document.addEventListener('playerIsUnlocked', event => {
+    console.log(event);
+});
+
+function lockAll() {
+    fetch('http://localhost:5000/game/lock', {
+        method: 'POST'
+    }).then(response => {
+        if (response.ok) {
+            alert('Tous les buzzers sont verrouillés.');
+        }
+    }).catch(err => {
+        console.error('Erreur:', err);
+    });
+}
+
+// Fonction pour déverrouiller tous les buzzers
+function unlockAll() {
+    fetch('http://localhost:5000/game/unlock', {
+        method: 'POST'
+    }).then(response => {
+        if (response.ok) {
+            alert('Tous les buzzers sont déverrouillés.');
+        }
+    }).catch(err => {
+        console.error('Erreur:', err);
+    });
+}
+
+// Fonction pour reset la partie
+function resetGame() {
+    fetch('http://localhost:5000/game/reset', {
+        method: 'POST'
+    }).then(response => {
+        if (response.ok) {
+            alert('La partie est réinitialisée.');
+        }
+    }).catch(err => {
+        console.error('Erreur:', err);
+    });
+}
+
+function getBuzzersList() {
+    fetch('http://localhost:5000/buzzers')
+    .then(response => {
+        if (response.ok) {
+            return response.json;
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
+}
