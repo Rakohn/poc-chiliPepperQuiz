1aa88b75cd4f728bfd7fc3cf53a84268bff59878
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2025-01-21 22:20:56 +0100

    implement teamReference.js, implements a function that return team reference

---
+++ js/teamReference.js
@@ -0,0 +1,10 @@
+function getTeamsReference() {
+    return {
+        2: "Roasted Hell",
+        3: "Caboom Tracer",
+        4: "Da'Bomb Ground Zero",
+        5: "Lucifer's Last Blast",
+        6: "Carolina Reaper",
+        7: "Zombie Snot Hellfire"
+    };
+}
