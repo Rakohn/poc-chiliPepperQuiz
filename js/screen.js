324aa78be2519926edbe438fb3d005aeeb416afe
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2025-01-22 02:46:35 +0100

    Refactor jingle launch script

--- js/screen.js
+++ js/screen.js
@@ -1,39 +1,27 @@
-const channel = new BroadcastChannel('jingleChannel');
+const jingleChannel = new BroadcastChannel('jingleChannel');
 const videoContainer = document.getElementById('videoContainer');
 const videoPlayer = document.getElementById('videoPlayer');
 const videoSource = document.getElementById('videoSource');
-const scoreTable = document.getElementById('scoreTable');
+const containerTeam = document.getElementById('team-container');
 
-channel.onmessage = (event) => {
+// Gestion des jingles
+jingleChannel.onmessage = (event) => {
     const { action, jingleId } = event.data;
 
     if (action === 'play') {
-        // Masquer le tableau des scores avec animation
-        scoreTable.classList.add('hidden');
+        // Masquer les cartes et afficher le lecteur vidéo
+        containerTeam.classList.add('hidden');
+        videoContainer.classList.remove('hidden');
 
-        // Attendre la fin de l'animation avant d'afficher la vidéo
-        setTimeout(() => {
-            videoContainer.style.display = 'block';
-            videoSource.src = `video/${jingleId}.mp4`;
-            videoPlayer.load();
-            videoPlayer.play();
-
-            // Passer en plein écran
-            if (videoContainer.requestFullscreen) {
-                videoContainer.requestFullscreen();
-            } else if (videoContainer.mozRequestFullScreen) {
-                videoContainer.mozRequestFullScreen();
-            } else if (videoContainer.webkitRequestFullscreen) {
-                videoContainer.webkitRequestFullscreen();
-            } else if (videoContainer.msRequestFullscreen) {
-                videoContainer.msRequestFullscreen();
-            }
-        }, 800); // Correspond au temps de l'animation CSS
+        // Charger et lire la vidéo
+        videoSource.src = `video/${jingleId}.mp4`;
+        videoPlayer.load();
+        videoPlayer.play();
     }
 };
 
-// Réaffiche le tableau après la fin de la vidéo
+// Réafficher les cartes à la fin du jingle
 videoPlayer.onended = () => {
-    videoContainer.style.display = 'none';
-    scoreTable.classList.remove('hidden');
+    videoContainer.classList.add('hidden');
+    containerTeam.classList.remove('hidden');
 };
